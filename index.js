7696f70bd8a0c98060b15850ca1e42ff49d1448d
Jeremy D <jeremy.dormitzer@gmail.com>
2016-03-01 13:53:48 -0700

    cities generate names, adjectives, and descriptors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var Twitter = require('twitter');
 var RestClient = require('node-rest-client').Client;
 var Chance = require('chance');
+var	pluralize = require('pluralize');
 
 var client = new Twitter({
   consumer_key: 'XIuxju45DPPRjGslLXQAD5vbE',
@@ -14,16 +15,78 @@ var restClient = new RestClient();
 var chance = new Chance();
 
 function generateCityDescription() {
-	var desc = "";
-	var name = chance.city();
-	
-	console.log(desc);
-	return desc;
+	var city = GenerateCity(function(city) {
+		console.log(city);
+	});
 }
 
-//setInterval(generateCityDescription, 1000);
+var Wordnik = {
+	getCommonNoun : function(plural, callback) {
+		restClient.get("http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=true&includePartOfSpeech=noun&excludePartOfSpeech=proper-noun&minCorpusCount=400&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=-1&maxLength=-1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5",
+			function(data, response) {
+				var noun = data.word;
+				if (plural) noun = pluralize(data.word, 2);
+				else noun = pluralize(data.word, 1);
+				callback(noun);
+		});
+	},
+	getAdjective : function(callback) {
+		restClient.get("http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=true&includePartOfSpeech=adjective&minCorpusCount=400&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=-1&maxLength=-1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5",
+			function(data, response) {
+				callback(data.word);	
+		});
+	},
+	getVerb : function(callback) {
+		restClient.get("http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=false&includePartOfSpeech=verb-transitive&minCorpusCount=-1&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=-1&maxLength=-1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5",
+			function(data, response) {
+				callback(data.word);
+		});
+	}
+};
+
+function GenerateCity(callback) {
+	var city = {};
+	city.name = chance.city();
+	
+	generateDescriptor(function(descriptor) {
+		city.descriptor = descriptor;
+		generateAdjective(function(adjective) {
+			city.adjective = adjective;
+			callback(city);
+		});
+	});
+};
+
+function generateDescriptor(callback) {
+	var numNouns = Math.floor((Math.random() * 2) + 1);
+	Wordnik.getCommonNoun(true, function(noun1) {
+		if (numNouns > 1) {
+			Wordnik.getCommonNoun(true, function(noun2) {
+				callback("city of " + noun1 + " and " + noun2);
+			});
+		}
+		else {
+			callback("city of " + noun1);
+		}
+	});
+}
+
+function generateAdjective(callback) {
+	Wordnik.getAdjective(function(adjective){
+		callback(adjective);
+	});
+}
+
+function generateClause(callback) {
+	
+}
+
+
+setInterval(generateCityDescription, 1000);
 
 /*
 City descriptions should be generated in the format "In the <adjective> city of <name>, <clause>", "<clause> in the the <adjective> city of <name>, or <name> a city of <description>"
 A clause looks like "<adjective><noun><verb><adjective><noun>"
+
+a city has a descriptor ("city of <blank> and <blank>"), an adjective, and a clause
 */
